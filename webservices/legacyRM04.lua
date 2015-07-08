edab29e366ba06d692c576e37661c9069486cf9e
Tom Clement <mr@tomclement.nl>
2015-07-08 18:01:47 +0200

    Moved sendfile to common repo

--- webservices/legacyRM04.lua
+++ webservices/legacyRM04.lua
@@ -5,20 +5,27 @@ local pin = 7
 
 -- Legacy support emulating HLK-RM04 module behaviour
 local function legacyRM04(connection, request)
-    local match = string.match(request, "gpio2=(??[0-9]?.?[0-9]*)")
+    print(request)
+    local match = string.match(request, "gpio2=(%d)")
 
     -- RM04 relay modules use inverted on/off states
     -- so if state is 1 we need to turn off, if state
     -- is 0 we need to turn on
-    if match == nil then
-        return false
-    elseif match == "1" then
+    if match == "1" then
+        print "Found match for 1"
         pwmDimmer.fadeTo(pin, 0)
         sendHttpResponse(connection, "Ok")
+        return true
     elseif match == "0" then
+        print "Found match for 0"
         pwmDimmer.fadeTo(pin, 1023)
         sendHttpResponse(connection, "Ok")
-    elseif match == "?" or match == "%3" then
+        return true
+    end
+
+    match = string.match(request, "gpio2=(...)%s")
+    if match == "?" or match == "%3" then
+        print "Found match for getDuty"
         local state
         if pwmDimmer.getDuty(pin) > 0 then
             state = 0
@@ -26,9 +33,10 @@ local function legacyRM04(connection, request)
             state = 1
         end
         sendHttpResponse(connection, "at+gpio2=? " .. state)
+        return true
     end
 
-    return true
+    return false
 end
 
 return legacyRM04
