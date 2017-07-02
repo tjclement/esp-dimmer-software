4ab317de379d7fead150ea9240dd28d6a63d20b5
Manuel Domke <domke@denic.de>
2017-07-02 17:23:02 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 wifiSettings = require("wifiSettings")
 
 autoWifi  = require("autoWifi")
-autoWifi.setup(wifiSettings.ssid, wifiSettings.key, {ip=wifiSettings.ip, netmask=wifiSettings.netmask, gateway=wifiSettings.gateway)
+autoWifi.setup(wifiSettings.ssid, wifiSettings.key, {ip=wifiSettings.ip, netmask=wifiSettings.netmask, gateway=wifiSettings.gateway})
 
 updateServer = require("updateServer")
 updateServer.start()
