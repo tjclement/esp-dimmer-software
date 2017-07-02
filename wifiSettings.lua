c1cd92e1ba2ec5da7419b71935d089546bb3d93e
Manuel Domke <domke@denic.de>
2017-07-02 15:22:31 +0200

    Added IP, Netmask and gateway

--- wifiSettings.lua
+++ wifiSettings.lua
@@ -1,6 +1,9 @@
 local wifiSettings = {}
 
-wifiSettings.wifi_ssid = "MySSID"
-wifiSettings.wifi_key = "MyPassword"
+wifiSettings.ssid = "MySSID"
+wifiSettings.key = "MyPassword"
+wifiSettings.ip = "192.168.1.230"
+wifiSettings.netmask = ""
+wifiSettings.gateway = "192.168.1.1"
 
 return wifiSettings
